032bd95c4a4788f68a2f2bb2210bf9a3a357ca16
OlivierWizi <olivier@wizi.eu>
2019-12-04 13:59:46 +0100

    méthode de répupération des fichiers signé

--- src/WiziYousignClient/WiziSignClient.php
+++ src/WiziYousignClient/WiziSignClient.php
@@ -54,6 +54,20 @@ class WiziSignClient
     }
 
     /**
+     * @param $apikey
+     */
+    public function setMember($member){
+        $this->member = $member;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMember(){
+        return $this->member;
+    }
+
+    /**
      * @return mixed
      */
     public function getIdfile(){
@@ -65,6 +79,47 @@ class WiziSignClient
      */
     public function setIdfile($idfile){
         $this->idfile = $idfile;
+    }
+
+    /**
+     * @param $fileid
+     * @param $mode
+     * @return bool|string
+     */
+    public function downloadSignedFile($fileid,$mode){
+        $curl = curl_init();
+        if($mode == 'binary'){
+            $urlstr =  "https://staging-api.yousign.com/files/".$fileid."/download";
+        }else{
+            $urlstr =  "https://staging-api.yousign.com/files/".$fileid."/download";
+        }
+
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://staging-api.yousign.com/files/".$fileid."/download",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer ".$this->getApikey(),
+                "Content-Type: application/json"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return "cURL Error #:" . $err;
+        } else {
+            return $response;
+        }
     }
 
     /**
